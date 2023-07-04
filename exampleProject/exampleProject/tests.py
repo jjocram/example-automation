ad57c9f328447b6daae6bba3a837b3ffda774add
Marco Ferrati <marco.ferrati@protonmail.com>
2023-07-04 17:01:48 +0200

    Create example project with tests

---
+++ exampleProject/exampleProject/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class ExampleTest(TestCase):
+    def test_to_test_tests_are_working(self):
+        """
+        Assert always true, simple test to test that tests are working correctly
+        """
+        self.assertIs(True, True)
